f4d37f8559379fe880892fe5005d218efce30613
Oleksii Hytsiv <71726515+tarilo4ka@users.noreply.github.com>
2022-10-19 20:18:20 +0300

    :memo: Implement User model

---
+++ RecipeBook/Models/User.cs
@@ -0,0 +1,13 @@
+class User
+{
+    // id
+    public long Id { get; set; }
+    // username
+    public string Username { get; set; }
+    // email
+    public string Email { get; set; }
+    // password
+    public string Password { get; set; }
+    // image
+    public byte[] Image { get; set; }
+}
