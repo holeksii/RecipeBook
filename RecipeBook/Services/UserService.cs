a8d08c60f5cc8dd5063589af487538ce7de596a5
Oleksii Hytsiv <71726515+tarilo4ka@users.noreply.github.com>
2022-10-20 13:28:22 +0300

    :construction_worker: Implenet CRUD in UserService

--- RecipeBook/Services/UserService.cs
+++ RecipeBook/Services/UserService.cs
@@ -11,4 +11,46 @@ public class UserService
     {
         _dbContext = dbContext;
     }
+
+    public User AddUser(User user)
+    {
+        _dbContext.Users.Add(user);
+        _dbContext.SaveChanges();
+
+        return user;
+    }
+
+    public List<User>? GetUsers()
+    {
+        return _dbContext.Users.ToList();
+    }
+
+    public User? GetUser(long id)
+    {
+        return _dbContext.Find<User>(id);
+    }
+
+    public User? DeleteUser(User user)
+    {
+        if (_dbContext.Find<User>(user.Id) != null)
+        {
+            _dbContext.Remove(user);
+            _dbContext.SaveChanges();
+
+            return user;
+        }
+        return null;
+    }
+
+    public User? UpdateUser(User user)
+    {
+        if (_dbContext.Find<User>(user.Id) != null)
+        {
+            _dbContext.Update(user);
+            _dbContext.SaveChanges();
+
+            return user;
+        }
+        return null;
+    }
 }
