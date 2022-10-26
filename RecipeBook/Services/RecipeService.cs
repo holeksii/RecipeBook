f0455e39ba38e94ce6c023761ef349a052ad84bf
Oleksii Hytsiv <71726515+tarilo4ka@users.noreply.github.com>
2022-10-26 21:47:05 +0300

    :sparkles:Implement Recipe Service

---
+++ RecipeBook/Services/RecipeService.cs
@@ -0,0 +1,53 @@
+﻿namespace RecipeBook.Services;
+
+using RecipeBook.Data;
+using RecipeBook.Models;
+
+public class RecipeService
+{
+    private RecipeBookDbContext _dbContext;
+
+    public RecipeService(RecipeBookDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Recipe AddRecipe(Recipe recipe)
+    {
+        _dbContext.Recipes.Add(recipe);
+        _dbContext.SaveChanges();
+        return recipe;
+    }
+
+    public List<Recipe>? GetRecipes()
+    {
+        return _dbContext.Recipes.ToList();
+    }
+
+    public Recipe? GetRecipe(long id)
+    {
+        return _dbContext.Find<Recipe>(id);
+    }
+
+    public Recipe? DeleteRecipe(Recipe recipe)
+    {
+        if (_dbContext.Find<Recipe>(recipe.Id) != null)
+        {
+            _dbContext.Remove(recipe);
+            _dbContext.SaveChanges();
+            return recipe;
+        }
+        return null;
+    }
+
+    public Recipe? UpdateRecipe(Recipe recipe)
+    {
+        if (_dbContext.Find<Recipe>(recipe.Id) != null)
+        {
+            _dbContext.Update(recipe);
+            _dbContext.SaveChanges();
+            return recipe;
+        }
+        return null;
+    }
+}
