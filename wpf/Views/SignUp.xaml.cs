5b1981ae68aaca7de7aef73000c922c11285ae56
Ірина-Анастасія Байкенич <IRYNA-ANASTASIIA.BAIKENYCH@lnu.edu.ua>
2022-11-24 14:08:56 +0200

    Complete login

--- wpf/Views/SignUp.xaml.cs
+++ wpf/Views/SignUp.xaml.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -34,10 +35,37 @@ namespace wpf.Views
         {
 
             RegisterViewModel model = new RegisterViewModel();
-            model.Username = txtUsername.Text;
-            model.Password = txtPassword.Password.ToString();
-            model.ConfirmPassword = txtRepeatPassword.Password.ToString();
+            if(ValidateUserName(txtUsername.Text))
+            {
+                model.Username = txtUsername.Text;
+            }
+            else {
+                ShowMessageBox_Click("Invalid Username", "error");
+                return;
+            }
+            
+            if (ValidatePassword(txtPassword.Password.ToString()))
+            {
+                model.Password = txtPassword.Password.ToString();
+            }
+            else {
+                ShowMessageBox_Click("Invalid Password", "error");
+                return;
+            }
+           
+            if (txtPassword.Password.ToString() == txtRepeatPassword.Password.ToString())
+            {
+                model.ConfirmPassword = txtRepeatPassword.Password.ToString();
+            }
+            else {
+                ShowMessageBox_Click("Passwords do not match", "error");
+                return;
+            }
             AddToDB(model);
+            var mainWindow = new RecipeList();
+            var myWindow = Window.GetWindow(this);
+            myWindow.Close();
+            mainWindow.Show();
 
         }
 
@@ -45,32 +73,54 @@ namespace wpf.Views
         {
             try
             {
-                UserService servise = new UserService(_dbContext);
+                using RecipeBookDbContext _dbContext2 = new RecipeBookDbContext();
+
+                UserService servise = new UserService(_dbContext2);
 
                 servise.AddUser(new User(registerViewModel.Username, registerViewModel.Username, registerViewModel.Password, " "));
-                ShowMessageBox_Click_Success();
+                
+                ShowMessageBox_Click("Congratulations! Registration was successful!", "successful");
             }
-            catch
+            catch(Exception ex)
             {
-                ShowMessageBox_Click_Eror();
+                ShowMessageBox_Click("User with such Username already exists!", "error");
             }
             
         }
 
-        private void ShowMessageBox_Click_Success()
+        private void ShowMessageBox_Click(string msgtext, string txt)
         {
-            string msgtext = "Congratulations! Registration was successful!";
-            string txt = "successful";
             MessageBoxButton button = MessageBoxButton.OK;
             MessageBoxResult result = MessageBox.Show(msgtext, txt, button);      
         }
 
-        private void ShowMessageBox_Click_Eror()
+        //private void ShowMessageBox_Click_Eror()
+        //{
+        //    string msgtext = "User with such Username already exists!";
+        //    string txt = "error";
+        //    MessageBoxButton button = MessageBoxButton.OK;
+        //    MessageBoxResult result = MessageBox.Show(msgtext, txt, button);
+        //}
+
+        private bool ValidateUserName(string userName)
         {
-            string msgtext = "User with such Username already exists!";
-            string txt = "error";
-            MessageBoxButton button = MessageBoxButton.OK;
-            MessageBoxResult result = MessageBox.Show(msgtext, txt, button);
+            string pattern;
+            // start with a letter, allow letter or number, length between 3 to 12.
+            pattern = @"^[a-zA-Z][a-zA-Z0-9]{2,11}$";
+
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(userName );
+        }
+
+        private bool ValidatePassword(string pass)
+        {
+            string pattern;
+            // at least one lower case letter, at least one upper case letter
+            //not be lesser than 8 or greater than 15 characters, contain at least one numeric value
+            pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$";
+
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(pass);
         }
     }
 }
