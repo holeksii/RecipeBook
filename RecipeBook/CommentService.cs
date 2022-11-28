e3265227a87cf8dca3cf10f667dc90f453006936
ULiesnik <uljana042003@gmail.com>
2022-11-28 14:48:52 +0200

    Saving problems fixed

---
+++ RecipeBook/CommentService.cs
@@ -0,0 +1,62 @@
+﻿namespace RecipeBook.Services;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System;
+using RecipeBook.Data;
+using RecipeBook.Models;
+
+public class CommentService
+{
+    private RecipeBookDbContext _dbContext;
+
+    public CommentService(RecipeBookDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Comment? AddComment(string text, long recipeId)
+    {
+        if (_dbContext.Find<Recipe>(recipeId) != null)
+        {
+            Comment comment = new Comment(text, DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc));
+            _dbContext.Recipes.Include(r => r.Ingredients).Include(r => r.Likes).Include(
+                r => r.Comments).Include(r=>r.User).FirstOrDefault(
+                    r => r.Id==recipeId).Comments.Add(comment);
+            _dbContext.SaveChanges();
+            return comment;
+        }
+        return null;
+    }
+
+    public List<Comment>? GetComments(Recipe recipe)
+    {
+        return _dbContext.Find<Recipe>(recipe.Id)?.Comments.ToList();
+    }
+
+    public Comment? GetComment(long id)
+    {
+        return _dbContext.Find<Comment>(id);
+    }
+
+    public Comment? DeleteComment(Comment comment)
+    {
+        if (_dbContext.Find<Comment>(comment.Id) != null)
+        {
+            _dbContext.Remove(comment);
+            _dbContext.SaveChanges();
+            return comment;
+        }
+        return null;
+    }
+
+    public Comment? UpdateComment(Comment comment)
+    {
+        if (_dbContext.Find<Comment>(comment.Id) != null)
+        {
+            _dbContext.Update(comment);
+            _dbContext.SaveChanges();
+            return comment;
+        }
+        return null;
+    }
+}
---
+++ RecipeBook/LikeService.cs
@@ -0,0 +1,82 @@
+﻿namespace RecipeBook.Services;
+using RecipeBook.Data;
+using RecipeBook.Models;
+
+public class LikeService
+{
+    private RecipeBookDbContext _dbContext;
+
+    public LikeService(RecipeBookDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Like? GetLike(long userId, long recipeId){
+        return _dbContext.Set<Like>().FirstOrDefault(l => 
+            l.User.Id == userId && l.Recipe.Id == recipeId);
+    }
+
+    private Like? AddLikeForUser(long userId)
+    {
+        User? user = _dbContext.Find<User>(userId);
+        if (user != null)
+        {
+            Like like = new(DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc));
+            user.Likes.Add(like);
+            return like;
+        }
+        return null;
+    }
+
+    public Like? AddLikeForRecipe(long recipeId, long userId)
+    {
+        Recipe? recipe = _dbContext.Find<Recipe>(recipeId);
+        if (recipe != null)
+        {
+            Like like = AddLikeForUser(userId);
+            if(like != null)
+            {
+                recipe.Likes.Add(like);
+                _dbContext.SaveChanges();
+                return like;
+            }
+        }
+        return null;
+    }
+
+
+    public Like? DeleteLikeForUser(long userId, long likeId)
+    {
+        User? user = _dbContext.Find<User>(userId);
+        
+        if (user != null)
+        {
+            Like? like = user.Likes.Find(l => l.Id == likeId);
+            if (like != null)
+            {
+                user.Likes.Remove(like);
+                _dbContext.Users.Update(user);
+                _dbContext.SaveChanges();
+                return like;
+            }
+        }
+        return null;
+    }
+
+    public Like? DeleteLikeForRecipe(long recipeId, long likeId)
+    {
+        Recipe? recipe = _dbContext.Find<Recipe>(recipeId);
+        if (recipe != null)
+        {
+            Like? like = recipe.Likes.Find(l => l.Id == likeId);
+            if (like != null)
+            {
+                recipe.Likes.Remove(like);
+                _dbContext.Recipes.Update(recipe);
+                _dbContext.SaveChanges();
+                return like;
+            }
+        }
+        return null;
+    }
+}
